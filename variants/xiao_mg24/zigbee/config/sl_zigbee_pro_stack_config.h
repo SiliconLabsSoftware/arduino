72d740876c7378442ad2f539ddf8f56a554a418c
Tamas Jozsi <124884281+silabs-bozont@users.noreply.github.com>
2026-08-11 14:01:18 +0200

    Increase the Zigbee binding table size to 32
    Done to handle multiple Zigbee devices better
    at the same time.

--- variants/xiao_mg24/zigbee/config/sl_zigbee_pro_stack_config.h
+++ variants/xiao_mg24/zigbee/config/sl_zigbee_pro_stack_config.h
@@ -111,7 +111,7 @@
 // <o SL_ZIGBEE_BINDING_TABLE_SIZE> Binding Table Size <1-127>
 // <i> Default: 3
 // <i> The number of entries that the binding table can hold.
-#define SL_ZIGBEE_BINDING_TABLE_SIZE   10
+#define SL_ZIGBEE_BINDING_TABLE_SIZE   32
 
 // </h>
 
